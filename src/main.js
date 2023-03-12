80afc36c70dee2b989cca88c804f805db4ccea36
Neven DREAN <nevendrean@yahoo.fr>
2023-03-12 11:48:50 -0500

    vite + surge

---
+++ src/main.js
@@ -0,0 +1,38 @@
+const map = L.map("map").setView([51.5, 0], 10);
+L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+  attribution:
+    '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+  preferCanvas: true,
+}).addTo(map);
+
+const markersCanvas = new L.MarkersCanvas();
+
+const icon = L.icon({
+  iconUrl: "./src/marker2.png",
+  iconSize: [20, 32],
+  iconAnchor: [10, 0],
+});
+
+const markers = [];
+markersCanvas.addTo(map);
+
+[...Array(10).keys()].forEach((i) => {
+  const lat = 51 + Math.random();
+  const lng = Math.random();
+  const marker = L.marker([lat, lng], { icon })
+    .bindPopup("I Am " + i)
+    .on({
+      mouseover(e) {
+        this.openPopup();
+      },
+      mouseout(e) {
+        this.closePopup();
+      },
+    });
+
+  markers.push(marker);
+});
+
+markersCanvas.addMarkers(markers);
+
+L.marker([52, 0], { icon }).addTo(map);
