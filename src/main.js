705e2366a4e8b8288fb34f4719921a027b10991f
Neven DREAN <nevendrean@yahoo.fr>
2023-03-15 08:26:07 -0500

    refactor

--- src/main.js
+++ src/main.js
@@ -13,7 +13,7 @@ const markersCanvas = new L.MarkersCanvas();
 
 const icon = L.icon({
   iconUrl: marker2,
-  iconSize: [20, 32],
+  iconSize: [40, 62],
   iconAnchor: [10, 0],
 });
 
@@ -23,7 +23,7 @@ markersCanvas.addTo(map);
 const html = (i) =>
   `<p>I am ${i}</p><button type="button" id="reset-${i}">Reset</button>`;
 
-[...Array(10).keys()].forEach((i) => {
+[...Array(5).keys()].forEach((i) => {
   const lat = 51 + Math.random();
   const lng = Math.random();
   const marker = L.marker([lat, lng], { icon }).bindPopup(html(i));
@@ -43,3 +43,17 @@ function deleteMarker(marker) {
 }
 
 markersCanvas.addMarkers(markers);
+
+// draggable only if not into canvas
+const m = L.marker([51, 0], { draggable: true });
+m.addTo(map);
+
+map.on("click", (e) => {
+  const new_marker = L.marker(e.latlng, { icon }).bindPopup(html(10));
+  markersCanvas.addMarker(new_marker);
+  new_marker.on("click", () => {
+    document
+      .querySelector("button")
+      .addEventListener("click", () => deleteMarker(new_marker));
+  });
+});
